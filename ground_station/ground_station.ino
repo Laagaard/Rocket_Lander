ca564675a80494440d1d1aa67e89583db0b4815b
ntardy1 <tardy.nate@gmail.com>
2024-07-19 16:02:09 -0400

    Integrate XBee radio and LCD display code

--- ground_station/ground_station.ino
+++ ground_station/ground_station.ino
@@ -2,23 +2,44 @@
 DART Ground Station Software
 */
 
-#include <SoftwareSerial.h>
+#include <SoftwareSerial.h> // for XBee communication
+#include <LiquidCrystal.h> // for LCD display
 
 SoftwareSerial XBee(2,3); // RX pin, TX pin
 
 // Establish pin number for abort pin
-int abort_pin = 8;
+int abort_pin = 4;
+
+// Establish pin number for initiation pin
+int initiation_pin = 5;
+
+// Establish pin number for launch pin
+int launch_pin = 6;
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
 
 void  setup()
 {
   // Open serial communication
   Serial.begin(9600);
+
   // Open XBee communication
   XBee.begin(9600);
+
   // Set abort pin as input
   pinMode(abort_pin, INPUT);
+
   // Set built-in LED as output
   pinMode(LED_BUILTIN, OUTPUT);
+
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
 }
 
 void loop()
@@ -37,6 +58,13 @@ void loop()
     XBee.print(0);
     digitalWrite(LED_BUILTIN, LOW);
   }
+
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
 
   // Wait 250 ms
   delay(250);
