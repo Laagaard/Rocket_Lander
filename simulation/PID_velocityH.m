fe3679f0c462e0045cdfe8a204f57ed76c2e332a
Laagaard <163777254+Laagaard@users.noreply.github.com>
2024-08-21 11:08:31 -0400

    Delete simulation/PID_velocityH.m

--- simulation/PID_velocityH.m
+++
@@ -1,6 +0,0 @@
-function [outputArg1,outputArg2] = PID_velocityH(inputArg1,inputArg2)
-    %UNTITLED3 Summary of this function goes here
-    %   Detailed explanation goes here
-    outputArg1 = inputArg1;
-    outputArg2 = inputArg2;
-end
