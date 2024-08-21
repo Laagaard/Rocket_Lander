a8fd618bde8cbf71545767364109c25faf878265
Laagaard <163777254+Laagaard@users.noreply.github.com>
2024-08-21 11:02:00 -0400

    Add files via upload

---
+++ simulation/Parameters_Earth.m
@@ -0,0 +1,10 @@
+function  Parameters_Earth
+    % Specifies Earth acceleration, air density, and other constants
+
+    global g_accel rho
+
+    % =====================================
+    % standard temperature and pressure values
+    g_accel = 9.81; % (m/s^2)
+    rho = 1.225;    % (kg/m^2)
+end
---
+++ simulation/Parameters_Trajectory.m
@@ -0,0 +1,5 @@
+function Parameters_Trajectory
+    global DesiredRange DesiredCrashSpeed
+    DesiredRange = 50 % (m)
+    DesiredCrashSpeed = 0 % (m/s)
+end
