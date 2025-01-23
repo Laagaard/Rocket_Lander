e2ffc53144484af0764046ebfdae4041ea85bbe4
Kian Jamal <kian.jamal.321@gmail.com>
2025-01-23 12:57:45 -0500

    Edited against buildable source code

--- simulation/6DOF/Python/TVC/test_flight.py
+++ simulation/6DOF/Python/TVC/test_flight.py
@@ -1,4 +1,16 @@
 import sys
 sys.path.append('../')
 import setup
-print("Hello World")
+
+from rocketpy import Flight
+
+test_flight = Flight(
+    rocket = setup.DART_rocket, 
+    environment = setup.launch_site,
+    rail_length = 1.5, #TBR (m)
+    inclination = 90, #TBR (deg)
+    heading = 0, #(deg)
+    time_overshoot = True
+)
+
+test_flight.plots.trajectory_3d()
