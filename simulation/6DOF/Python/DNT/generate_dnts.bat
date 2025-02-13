d48ec32e2688cd098a11adefd5b805bc87aa5962
ntardy1 <95262466+ntardy1@users.noreply.github.com>
2025-02-12 19:14:59 -0500

    Update generate_dnts.bat
    Remove `minutes_list` and limit to calculation of optimal trajectory

--- simulation/6DOF/Python/DNT/generate_dnts.bat
+++ simulation/6DOF/Python/DNT/generate_dnts.bat
@@ -1,14 +1,9 @@
 @echo off
 set dates_list=02-23-2025
 set hours_list=11 12 13 14 15
-set minutes_list=00
 (for %%a in (%dates_list%) do (
     (for %%b in (%hours_list%) do (
-        (for %%c in (%minutes_list%) do (
-            python .\dataset_generation.py --date %%a --time %%b --location ROAR --automated True
-            python .\optimal_trajectory.py --date %%a --time %%b --location ROAR --automated True
-            python .\dnt_trajectories.py --date %%a --time %%b --location ROAR --automated True
-            python .\dnt_generation.py --date %%a --time %%b --location ROAR --automated True
-        ))
+        python .\dataset_generation.py --date %%a --time %%b --location ROAR --automated True
+        python .\optimal_trajectory.py --date %%a --time %%b --location ROAR --automated True
     ))
 ))
